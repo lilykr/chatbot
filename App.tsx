94cf2d46f280ce2bb41f35d0c070eed3704b695d
lilykr <lisa-lou.k@hotmail.fr>
2025-03-05 18:27:37 +0200

    minimal working example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import { GiftedChat, type IMessage } from "react-native-gifted-chat";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-export default function App() {
+export function App() {
+	const [messages, setMessages] = useState<IMessage[]>([]);
+
+	useEffect(() => {
+		setMessages([
+			{
+				_id: 1,
+				text: "Hello developer",
+				createdAt: new Date(),
+				user: {
+					_id: 2,
+					name: "React Native",
+					avatar: "https://placeimg.com/140/140/any",
+				},
+			},
+		]);
+	}, []);
+
+	const onSend = useCallback((messages: IMessage[]) => {
+		setMessages((previousMessages) =>
+			GiftedChat.append(previousMessages, messages),
+		);
+	}, []);
+
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<SafeAreaProvider>
+			<SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+				<GiftedChat
+					messages={messages}
+					onSend={(messages) => onSend(messages)}
+					user={{
+						_id: 1,
+					}}
+				/>
+			</SafeAreaView>
+		</SafeAreaProvider>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
