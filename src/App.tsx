75d33f1305cc1e1314c37d6a832d6ddeac07ee63
lilykr <lisa-lou.k@hotmail.fr>
2025-03-10 11:04:53 +0200

    Add error boundary to App component for improved error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { colors } from "./constants/colors";
 import { Chat } from "./features/chat/Chat";
+import ErrorBoundary from "react-native-error-boundary";
 
 SplashScreen.preventAutoHideAsync();
 
 export function App() {
 	return (
-		<>
+		<ErrorBoundary>
 			<StatusBar style="light" backgroundColor={colors.night} />
 			<SafeAreaProvider>
 				<Chat />
 			</SafeAreaProvider>
-		</>
+		</ErrorBoundary>
 	);
 }
