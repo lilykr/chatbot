5fdb3631c1b0d4fd4f4be7790da38034637afb48
lilykr <lisa.lou.kara@gmail.com>
2025-03-28 19:21:44 +0100

    Refactor history page and babel configuration for improved code clarity. Remove unused scripts and translation files related to i18n migration.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = (api) => {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: ["@babel/plugin-transform-class-static-block"],
-  };
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: ["@babel/plugin-transform-class-static-block"],
+	};
 };
