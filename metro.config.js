2e954bbb36709fdebc8f117f212fe0d21755186a
lilykr <lisa.lou.kara@gmail.com>
2025-03-31 18:15:26 +0200

    Update app.json to include appleTeamId, enhance metro.config.js for HTTPS support, and add new dependencies in package.json and yarn.lock.

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,20 @@ const { getDefaultConfig } = require("expo/metro-config");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Force server to use HTTPS and tunneling
+config.server = {
+    ...config.server,
+    useSSL: true,
+    enhanceMiddleware: (middleware) => {
+        return (req, res, next) => {
+            // Force the host header to be the tunnel URL if available
+            const tunnelHost = process.env.EXPO_TUNNEL_HOSTNAME;
+            if (tunnelHost) {
+                req.headers.host = tunnelHost;
+            }
+            return middleware(req, res, next);
+        };
+    },
+};
+
 module.exports = config;
